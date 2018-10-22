930e39d24fe15536fbf80f7d73e2e66af0c2e12c
mishka leonov <joethefina@ya.ru>
2018-10-22 23:54:19 +0300

    допилил ошибки переполнения, сделал закрытие попапа по клику вне окна, исправил декоративные волны, добавил webp версии картинок, автоматизировал спрайты на страницу

--- source/js/modal-open.js
+++ source/js/modal-open.js
@@ -10,11 +10,19 @@ for (var i = 0; i < basketLink.length; i++) {
     overlay.classList.add("modal-overlay-show");
   });
 
+  overlay.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+
+    overlay.classList.remove("modal-overlay-show");
+  });
+
   window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
       if (popup.classList.contains("modal-show")) {
         popup.classList.remove("modal-show");
+
         overlay.classList.remove("modal-overlay-show");
       }
     }
@@ -28,11 +36,19 @@ link.addEventListener("click", function (evt) {
   overlay.classList.add("modal-overlay-show");
 });
 
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+
+  overlay.classList.remove("modal-overlay-show");
+});
+
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (popup.classList.contains("modal-show")) {
       popup.classList.remove("modal-show");
+
       overlay.classList.remove("modal-overlay-show");
     }
   }
